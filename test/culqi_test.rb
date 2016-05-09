2f1e8202e4f834fc355d9bd0672f84d765b30f8a
Gustavo Beathyate <gustavo.bt@me.com>
2016-05-08 20:34:53 -0500

    Require version

--- test/culqi_test.rb
+++ test/culqi_test.rb
@@ -1,11 +1,8 @@
 require 'test_helper'
+require 'culqi/version'
 
 class CulqiTest < Minitest::Test
   def test_that_it_has_a_version_number
-    refute_nil ::Culqiruby::VERSION
-  end
-
-  def test_it_does_something_useful
-    assert false
+    refute_nil ::Culqi::VERSION
   end
 end
