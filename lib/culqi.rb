0a3699a0038e64e6ed5665cf9170149e7526d589
Augusto Samame <augustosamame@deseoestrellita.com>
2016-05-11 02:51:50 +0000

    version 1.1.2

--- lib/culqi.rb
+++ lib/culqi.rb
@@ -4,6 +4,9 @@ require 'culqi/client'
 
 module Culqi
   def self.default_client
+    variables = %w{CULQI_KEY CULQI_ENDPOINT CULQI_CODIGO_COMERCIO}
+    missing = variables - ENV.keys
+    raise "The following env vars are missing and are needed to use this gem: #{missing.join(', ')}." unless missing.empty?
     Client.new(ENV['CULQI_KEY'], ENV['CULQI_ENDPOINT'], ENV['CULQI_CODIGO_COMERCIO'])
   end
 end
