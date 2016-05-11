904bdeeed35a094444f6caf4112e308600747416
Augusto Samame <augustosamame@deseoestrellita.com>
2016-05-11 19:23:20 +0000

    version 1.1.9 fixed tests

--- test/encryptor_test.rb
+++ test/encryptor_test.rb
@@ -2,10 +2,9 @@ require 'test_helper'
 require 'culqi/encryptor'
 
 class EncryptorTest < Minitest::Test
-  TEST_KEY = '89d641e07b069fba48f9c1b00a565825d3b89a6f5b393ec39ba7bc38b156b803836137a40e12e89878db21eb53442c10d179bc9f2fc8520dfd1ff19ab55a3700'
-
+  ENV['CULQI_KEY'] = 'JlhLlpOB5s1aS6upiioJkmdQ0OYZ6HLS2+/o4iYO2MQ='
   def test_encrypt_decrypt
-    encryptor = Culqi::Encryptor.new(TEST_KEY)
+    encryptor = Culqi::Encryptor.new
     plaintext = 'test string'
     encrypted = encryptor.encrypt(plaintext)
 
